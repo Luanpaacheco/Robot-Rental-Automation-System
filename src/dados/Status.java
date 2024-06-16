e2661d8306230353351a51191d4cd62892b538fa
LuisTrein <21luiseduardo2005@gmail.com>
2024-06-16 18:50:50 -0300

    Implementando Enum

--- src/dados/Status.java
+++ src/dados/Status.java
@@ -1,20 +1,46 @@
 package dados;
 
 public enum Status {
-
 	CADASTRADA("CADASTRADA"),
 	EXECUTANDO("EXECUTANDO"),
 	FINALIZADA("FINALIZADA"),
-	CANCELADA("CANCELDA");
+	CANCELADA("CANCELADA");
 
 	private String nome;
 
-	private Status(String nome){
-		this.nome=nome;
+	private Status(String nome) {
+		this.nome = nome;
 	}
 
-	public String getNome(){
+	public String getNome() {
 		return nome;
 	}
 
+	public boolean permiteAlteracao() {
+		return this == CADASTRADA || this == EXECUTANDO;
+	}
+
+	public Status iniciarLocacao() {
+		if (this == CADASTRADA) {
+			return EXECUTANDO;
+		} else {
+			throw new IllegalStateException("Não é possível iniciar locação nesta situação.");
+		}
+	}
+
+	public Status finalizarLocacao() {
+		if (this == EXECUTANDO) {
+			return FINALIZADA;
+		} else {
+			throw new IllegalStateException("Não é possível finalizar locação nesta situação.");
+		}
+	}
+
+	public Status cancelarLocacao() {
+		if (this == CADASTRADA || this == EXECUTANDO) {
+			return CANCELADA;
+		} else {
+			throw new IllegalStateException("Não é possível cancelar locação nesta situação.");
+		}
+	}
 }
