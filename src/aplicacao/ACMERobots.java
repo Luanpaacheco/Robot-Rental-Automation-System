77008efd555645cfa4751f1de155c7b8ef574167
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-06-29 20:02:08 -0300

    sem erros

--- src/aplicacao/ACMERobots.java
+++ src/aplicacao/ACMERobots.java
@@ -2,20 +2,23 @@ package aplicacao;
 
 import dados.cliente.Cliente;
 import dados.robo.Robo;
-
+import dados.Locacao;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ACMERobots {
     private List<Robo> listaRobos = new ArrayList<>();
     private List<Cliente>clientes;
-
+    private ArrayList<Locacao> listaLocacoes = new ArrayList<>();
 
 
     public ACMERobots() {
         this.listaRobos = listaRobos;
         this.clientes = new ArrayList<>();
-
+        this.listaLocacoes = listaLocacoes;
     }
 
     public boolean adicionarRobo(Robo novoRobo) {
@@ -23,6 +26,37 @@ public class ACMERobots {
             return false;
         }
         return listaRobos.add(novoRobo);
+    }
+
+    public boolean adicionarLocacao(Locacao novaLocacao) {
+        for(Locacao l : listaLocacoes) {
+            if(novaLocacao.getNumero() == l.getNumero()) {
+                return false;
+            }
+        }
+        return listaLocacoes.add(novaLocacao);
+    }
+
+    public boolean adicionarRoboNaLocacao(int numero, Robo novoRobo) {
+        Locacao l = listaLocacoes.get(numero);
+        for(Robo robo : l.getListaRobos()) {
+            if(robo == novoRobo) {
+                return false;
+            }
+        }
+        l.adicionaRobos(novoRobo);
+        return true;
+        //return false;
+    }
+
+    public Date dataConvertida(String dataString) {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date data = formato.parse(dataString);
+            return data;
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
     public Robo consultaId(int id) {
@@ -53,10 +87,29 @@ public class ACMERobots {
         } return null;
     }
 
+    /*
+       public Cliente consultaCodigo(int codigo) {
+        Cliente cliente = listaClientes.stream()
+                .filter(m -> m.getCodigo() == codigo)
+                .findFirst()
+                .orElse(null);
+        if (cliente != null) {
+            return cliente;
+        } else {
+            return null;
+        }
+    }
+     */
 
     public List<Robo> getListaRobos() {
         return listaRobos;
     }
 
+    public List<Cliente> getListaClientes() {
+        return clientes;
+    }
+    public ArrayList<Locacao> getListaLocacoes() {
+        return listaLocacoes;
+    }
 
 }
